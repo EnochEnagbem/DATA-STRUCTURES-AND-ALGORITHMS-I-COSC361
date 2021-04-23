0c9b449139c39f3e56d3f23c7d3c5de7f0f9801a
Enoch Enagbem <enoch.kumanyenu99@yahoo.com>
2021-04-23 15:10:02 +0000

    Create Main.java

---
+++ Trees/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Trees binaryTree = new Trees();
+
+	    binaryTree.insert(5);
+        binaryTree.insert(3);
+        binaryTree.insert(2);
+        binaryTree.insert(1);
+        binaryTree.insert(6);
+
+    }
+}
