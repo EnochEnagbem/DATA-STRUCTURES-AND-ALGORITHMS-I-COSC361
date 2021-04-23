910c9e1920dd44eeba3d06b347df7ed5331ca196
Enoch Enagbem <enoch.kumanyenu99@yahoo.com>
2021-04-23 15:15:49 +0000

    Create Node.java

---
+++ BinaryTree/src/com/company/Node.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Node{
+    Node left;
+    Node right;
+    int data;
+
+    //Constructor
+    Node(int newData){
+        left=null;
+        right=null;
+        data=newData;
+    }
+}
